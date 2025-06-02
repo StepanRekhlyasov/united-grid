5dcc46e30f3b0e2e26a7ba7b54d7835368de5c07
Stepan <srekhlyasov@gmail.com>
2025-06-02 17:49:45 +0200

    fix scroll

--- UnitedGridChat/src/components/widgets/chat/ChatMessages.vue
+++ UnitedGridChat/src/components/widgets/chat/ChatMessages.vue
@@ -4,7 +4,7 @@
  * @description Отображает список всех сообщений чата, используя компонент ChatMessage
  */
 <template>
-  <div class="messages-container">
+  <div class="messages-container" ref="wrapper">
     <ChatMessage v-for="(message, index) in messages" :key="index" :message="message" />
   </div>
 </template>
@@ -13,9 +13,20 @@
 import { useChatStore } from '@/store/chat';
 import ChatMessage from './ChatMessage.vue';
 import { storeToRefs } from 'pinia';
+import { ref, watch, nextTick } from 'vue';
 
+const wrapper = ref<Element>();
 const chatStore = useChatStore();
 const { messages } = storeToRefs(chatStore);
+/**
+ * Автоматически прокручивает список сообщений вниз при добавлении новых сообщений
+ */
+watch(messages.value, async () => {
+  if(wrapper.value){
+    await nextTick()
+    wrapper.value.scrollTop = wrapper.value.scrollHeight;
+  }
+});
 </script>
 
 <style scoped>
@@ -23,5 +34,10 @@ const { messages } = storeToRefs(chatStore);
   overflow: auto;
   height: 100%;
   padding: 1rem;
+  flex-direction: column;
+  display: flex;
+}
+.messages-container > :first-child {
+  margin-top: auto;
 }
 </style>
